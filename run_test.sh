3c0c936df0278ada5c0e6852acc3c90a00bd5766
Stephan Zaat <stephan@zosc.nl>
2016-02-16 17:04:50 +0100

    BEH-68 Added test

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -vs "http://localhost:9090/?p=1079WJ&h=128&hl=h" 2>&1 | grep "Location: https://atlas.amsterdam.nl/#/?zoek=1079WJ%20128%20h"
+curl -vs "http://localhost:9090/?p=1079WJ&h=128&hl=h" 2>&1 | grep -q "Location: https://atlas.amsterdam.nl/#/?zoek=1079WJ%20128%20h" ; echo $?
