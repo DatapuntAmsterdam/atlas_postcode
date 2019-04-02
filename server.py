b4501f7485c1c4641f1d4a2642aa5e8dafbab12a
kelvinpaska <k.paska@amsterdam.nl>
2019-04-02 11:05:48 +0200

    incommented test phase

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ def get_docker_host():
 app = Flask(__name__)
 
 db_host = os.environ.get('DATABASE_PORT_5432_TCP_ADDR', get_docker_host())
-db_port = os.environ.get('DATABASE_PORT_5432_TCP_PORT', 5454)
+db_port = os.environ.get('DATABASE_PORT_5432_TCP_PORT', 5432)
 db_user = os.environ.get('DB_USER', 'postgres')
 db_name = os.environ.get('DB_NAME', 'bag')
 db_pass = os.environ.get('DB_PASS', 'insecure')
